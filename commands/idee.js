4d5c942051aae207c2e7f207fdd68790a1eb1f60
vvikkttorr <59095084+vvikkttorr@users.noreply.github.com>
2020-02-28 12:58:51 +0100

    Update idee.js

--- commands/idee.js
+++ commands/idee.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Ingezonden door: ", message.author);
  
     // Vind het kanaal.
-    var ideeChannel = message.guild.channels.find(`name`, ":『:bulb:』ideeën");
+    var ideeChannel = message.guild.channels.find(`name`, "『:bulb:』ideeën");
     if (!ideeChannel) return message.guild.send("Kan het kanaal niet vinden");
  
     // Verzend het bericht en voeg er reacties aan toe.
