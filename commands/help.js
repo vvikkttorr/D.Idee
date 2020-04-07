08984223df051f7fb20d2d650310076977579611
vvikkttorr <59095084+vvikkttorr@users.noreply.github.com>
2020-04-07 15:53:06 +0200

    init

---
+++ commands/help.js
@@ -0,0 +1,45 @@
+
+const discord = require("discord.js");
+ 
+module.exports.run = async (bot, message, args) => {
+var commandsList = [];
+ 
+    bot.commands.forEach(command => {
+ 
+        var item = {
+ 
+            name: command.help.name,
+            description: command.help.description,
+            // category: command.help.category
+ 
+        }
+ 
+        commandsList.push(item);
+ 
+    });
+ 
+    // console.log(commandsList);
+ 
+    var prefix = botConfig.prefix;
+    var response = "";
+ 
+    for (var i = 0; i < commandsList.length; i++) {
+ 
+        response += `${prefix}${commandsList[i]["name"]} - ${commandsList[i]["description"]} \r\n`;
+ 
+    }
+ 
+    message.author.send(response).then(() => {
+ 
+        message.channel.send("Al de commando's staan in je privé berichten! :mailbox_with_mail:");
+ 
+    }).catch(() => {
+ 
+        message.channel.send("Je privé berichten staan uit geschakeld, je hebt geen hulp ontvangen");
+ 
+    });
+}
+module.exports.help = {
+    name: "hel",
+    description: "Heb je een idee. Zet het dan hier en misschien passen we het toe."
+}    
